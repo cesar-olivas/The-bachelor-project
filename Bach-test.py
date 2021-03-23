271b5daf17bb22aa6fce051e5a7a0dd79acff209
cesar-olivas <81172679+cesar-olivas@users.noreply.github.com>
2021-03-22 18:24:34 -0700

    Update Bach-test.py

--- Bach-test.py
+++ Bach-test.py
@@ -5,7 +5,7 @@ import nltk
 import random
 from nltk.corpus import stopwords
 stopwords = nltk.corpus.stopwords.words("english")
-#NEWNEWNEWNEWNEWNEWNEWNEWNEWNEWNEWNEWN
+
 #Txt file of show transcripts  (Seasonn 14) sourced from:
 #https://transcripts.foreverdreaming.org/viewforum.php?f=292
 myfile = "/Users/Cesar R. Olivas/Desktop/py projects/Bachelorette.txt"  # Mac users should leave out C:
@@ -14,18 +14,18 @@ bac_string = str(rtxt) # convert to string text
 tokens = nltk.word_tokenize(rtxt) #tokenize
 
 words = random.choice(tokens)
-print("random word:", words)
+print("random word:", words) #produces a random word from data
 
 bigram = nltk.bigrams(tokens) #create bigrams
 cfd = nltk.ConditionalFreqDist(bigram)
 
 word = random.choice(tokens)
-print("random word:", word)
+print("random word:", word) #produces a random word from bigram tokens
 
 
-words = tokens[:400]
-print(words)
-#To create bigrams, we will iterate through the list of the words with two indices,
+words = tokens[:5000]
+#print(words) #prints words value
+#Iterates through the list of the words with two indices,
 # one of which is offset by one:
 bigrams = [b for b in zip(words[:-1], words[1:])]
 print(bigrams)
@@ -33,12 +33,10 @@ print(bigrams)
 condition = 'love'
 next_words = [bigram[1] for bigram in bigrams
           if bigram[0].lower() == condition]
-print("next words:", next_words)
-#We have now found all of the possible words that can follow the condition “the” according to our corpus:
+#Possible words that can follow the condition “the” according to the data
+print("next word for love:", next_words)
 
-#in order to predict the next word, what we really want to compute is what
-# is the most likely next word out of all of the possible next words.
-
+#most likely next word out of all of the possible next words.
 from collections import defaultdict
 cfd = defaultdict(lambda: defaultdict(lambda: 0))
 for i in range(len(words) - 2):  # loop to the next-to-last word
@@ -49,4 +47,8 @@ print({k: dict(v) for k, v in dict(cfd).items()})
 
 #So, what’s the most likely word to follow 'the'?
 print("next likely word: ", max(cfd['the']))
+#this is very interesting
+print("next likely word: ", max(cfd['so']))
+
+
 
